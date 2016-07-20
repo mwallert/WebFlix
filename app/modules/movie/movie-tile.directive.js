5bcd54cffd41befd777e64f57c6f5967f511e94b
Michael Wallert <michael.wallert@hotmail.com>
2016-07-19 19:01:33 -0700

    pushing stuff from class

--- app/modules/movie/movie-tile.directive.js
+++ app/modules/movie/movie-tile.directive.js
@@ -6,8 +6,18 @@
     .directive('movieTile', movieTile);
 
   function movieTile() {
+    function controller() {
+
+    }
     return {
       restrict: 'E',
+      controller: controller,
+      controllerAs: 'movietile',
+      bindToController: true,
+      scope: {
+        movie: '=',
+        selectedMovie: '='
+      },
       templateUrl: 'build/partials/movie/movie-tile.html'
     };
   }
